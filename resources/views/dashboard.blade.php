21b4e9c55a47610e08c37346e34e67a4ee4efcd3
fhiara06 <fathiara041006@gmail.com>
2025-07-04 15:02:19 +0700

    forumm diskusii

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Home') }}
         </h2>
-    </x-slot>
+    </x-slot>    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto py-12 px-4">
